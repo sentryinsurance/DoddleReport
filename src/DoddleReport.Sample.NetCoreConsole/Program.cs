db7da5e67829310231925606c8d450bf257c1b22
Andrew Konkol <069508@sentry.com>
2022-05-23 16:56:42 -0500

    Got a .NET 6 console app generating an HTML report using the .NET Standard version of DoddleReport

--- src/DoddleReport.Sample.NetCoreConsole/Program.cs
+++ src/DoddleReport.Sample.NetCoreConsole/Program.cs
@@ -1,2 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using DoddleReport;
+using DoddleReport.Writers;
+
+// See https://aka.ms/new-console-template for more information
+
 Console.WriteLine("Hello, World!");
+
+
+var products = GetAll();
+
+var report = new Report(products.ToReportSource());
+var totalProducts = products.Count();
+var totalOrders = products.Sum(p => p.OrderCount);
+
+// Customize the Text Fields
+report.TextFields.Title = "Products Report";
+report.TextFields.SubTitle = "This is a sample report showing how Doddle Report works";
+report.TextFields.Footer = "Copyright 2011 &copy; The Doddle Project";
+report.TextFields.Header = string.Format(@"
+    Report Generated: {0}
+    Total Products: {1}
+    Total Orders: {2}
+    Total Sales: {3:c}", DateTime.Now, totalProducts, totalOrders, totalProducts * totalOrders);
+
+
+// Render hints allow you to pass additional hints to the reports as they are being rendered
+report.RenderHints.BooleanCheckboxes = true;
+
+
+// Customize the data fields
+report.DataFields["Id"].Hidden = true;
+report.DataFields["Price"].DataFormatString = "{0:c}";
+report.DataFields["LastPurchase"].DataFormatString = "{0:d}";
+
+var writer = new HtmlReportWriter();
+using var outputStream = System.IO.File.Create("c:\\temp\\test.html");
+
+writer.WriteReport(report, outputStream);
+
+Console.WriteLine("Done!");
+Console.ReadLine();
+
+static List<Product> GetAll()
+{
+    var rand = new Random();
+    return Enumerable.Range(1, 1000)
+        .Select(i => new Product
+        {
+            Id = i,
+            Name = "Product " + i,
+            Description = "Some random lines of text",
+            Price = rand.NextDouble() * 100,
+            OrderCount = rand.Next(1000),
+            LastPurchase = DateTime.Now.AddDays(rand.Next(1000)),
+            UnitsInStock = rand.Next(0, 2000)
+        })
+        .ToList();
+}
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+    public DateTime LastPurchase { get; set; }
+    public int OrderCount { get; set; }
+    public int UnitsInStock { get; set; }
+}
