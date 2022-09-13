fae25a7d30282de93641b0c0027bc958f446fc9f
Konkol Andrew <andrew.konkol@sentry.com>
2022-09-13 14:50:05 -0500

    Updating package versions to 2.0.0-beta01

--- nuget/CreatePackages.bat
+++ nuget/CreatePackages.bat
@@ -4,12 +4,13 @@
 @rd /S /Q packages
 @mkdir packages
 
-nuget restore ..\src\DoddleReport.sln
-nuget pack ..\src\DoddleReport\DoddleReport.csproj -prop Configuration=Release -Build -Symbols -OutputDirectory .\packages
-nuget pack ..\src\DoddleReport.AbcPdf\DoddleReport.AbcPdf.csproj -prop Configuration=Release -Build -Symbols -OutputDirectory .\packages
-nuget pack ..\src\DoddleReport.iTextSharp\DoddleReport.iTextSharp.csproj -prop Configuration=Release -Build -Symbols -OutputDirectory .\packages
-nuget pack ..\src\DoddleReport.OpenXml\DoddleReport.OpenXml.csproj -prop Configuration=Release -Build -Symbols -OutputDirectory .\packages
+rem Most packages are now built automatically due to settings in the csproj files
+rem nuget restore ..\src\DoddleReport.sln
+rem nuget pack ..\src\DoddleReport\DoddleReport.csproj -prop Configuration=Release -Build -Symbols -OutputDirectory .\packages
+rem nuget pack ..\src\DoddleReport.AbcPdf\DoddleReport.AbcPdf.csproj -prop Configuration=Release -Build -Symbols -OutputDirectory .\packages
+rem nuget pack ..\src\DoddleReport.iTextSharp\DoddleReport.iTextSharp.csproj -prop Configuration=Release -Build -Symbols -OutputDirectory .\packages
+rem nuget pack ..\src\DoddleReport.OpenXml\DoddleReport.OpenXml.csproj -prop Configuration=Release -Build -Symbols -OutputDirectory .\packages
 nuget pack ..\src\DoddleReport.Web\DoddleReport.Web.csproj -prop Configuration=Release -Build -Symbols -OutputDirectory .\packages
 
-nuget pack ..\src\DoddleReport.Sample.Web\DoddleReport.Sample.Mvc.nuspec -OutputDirectory .\packages
+rem nuget pack ..\src\DoddleReport.Sample.Web\DoddleReport.Sample.Mvc.nuspec -OutputDirectory .\packages
 
